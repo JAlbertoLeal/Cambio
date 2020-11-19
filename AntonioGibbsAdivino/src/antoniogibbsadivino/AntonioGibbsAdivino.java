6be823bbc7f0386ceeaf768a7461caf3315adbf1
JAlbertoLeal <juan.selfaleal@gmail.com>
2020-11-19 23:11:53 +0100

    Fusionando rama

--- AntonioGibbsAdivino/src/antoniogibbsadivino/AntonioGibbsAdivino.java
+++ AntonioGibbsAdivino/src/antoniogibbsadivino/AntonioGibbsAdivino.java
@@ -4,8 +4,7 @@ package antoniogibbsadivino;
 import java.util.Scanner;
 
 public class AntonioGibbsAdivino {
-
-  
+	//El programa funciona. Revisado por Juan Alberto! 
     public static void main(String[] args) {
         
         int n = 10,cont = 10;
